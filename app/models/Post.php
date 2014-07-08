63ab1ce77282301345e19fc512bffdf9fa03f1f3
Daniel Jimenez <danieljimenez@Daniels-MacBook-Air.local>
2014-07-08 11:57:47 -0500

    Updated this to purify dirty HTML

--- app/models/Post.php
+++ app/models/Post.php
@@ -1,14 +1,37 @@
 <?php
 
-class Post extends Eloquent {
+class Post extends BaseModel {
 
 	// The bd table this model relates to
     protected $table = 'posts';
+
+    protected $imgDir = 'img-upload';
 
     // Validation rules for our model properties
     static public $rules = [
     	'title' => 'required|max:100',
     	'body' => 'required|max:10000'
     ];
+
+    public function user()
+	{
+	    return $this->belongsTo('User');
+	}
+
+    public function addUploadedImage($image)
+    {
+        $systemPath = public_path() . '/' . $this->imgDir . '/';
+        $imageName = $this->id . '-' . $image->getClientOriginalName();
+        $image->move($systemPath, $imageName);
+        $this->img_path = '/' . $this->imgDir . '/' . $imageName;
+    }
+
+    public function renderBody() 
+    {
+        $dirtyHTML = Parsedown::instance()->parse($this->body);
+        $config = HTMLPurifier_Config::createDefault();
+        $purifier = new HTMLPurifier($config);
+        return $purifier->purify($dirtyHTML);
+    }
 
 }
