42372c441ff57939bcf3f91e9019b4f7d095ba30
Danny Jimenez <dannyalxndr@gmail.com>
2014-07-11 09:18:23 -0500

    changed blog index layout

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -2,23 +2,26 @@
 
 @section('content')
 
-	<table class="table table-hover">
-		<tr>
-		 	<th>All the Posts</th>
-		 	<th>Time created</th>
-		</tr>
-		
-		 @foreach ($posts as $post)
-		 <tr>
-		 	<td>{{ link_to_action ('PostsController@show', $post->title, array($post->id)) }}</td>
-		 	<td>{{{ $post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i:s A') }}}</td>
-		 	<td>{{ link_to_action('PostsController@edit', 'Edit', array($post->id), array('class' => 'btn btn-default btn-sm')) }}</td>
-		 </tr>
-		 @endforeach
-		
-	</table>
-	{{ $posts->links() }}
-	 <hr>
-	{{ link_to_action('PostsController@create', 'NewPost') }}
+	<div class="row col-xs-12" id="text-header">
+		<h1 id="indexHeader">Blog Entries<sup></sup></h1>
+	</div>
+		<div id="indexPosts" class="col-md-8">
+			@foreach ($posts as $post)
+			<h2>{{ link_to_action ('PostsController@show', $post->title, array($post->id)) }}</h5>
+			<h5>{{{ $post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i:s A') }}}</td>
+			 	<br>
+			 	<div class="blogPost">
+					<p>{{ substr($post->body, 0, 100) }}</p>
+				</div>
+				<hr />
+			 	@if (Auth::check())
+			 		<td>{{ link_to_action('PostsController@edit', 'Edit', array($post->id), array('class' => 'btn btn-default btn-sm')) }}</td>
+			 	@endif
+		    @endforeach
+		</div>
+	<div id="paginationLinks">
+		{{ $posts->links() }}
+	</div>
+	
 
 @stop
