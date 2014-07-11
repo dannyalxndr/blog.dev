d666a5932ee4057863d40374a61243f356419750
Danny Jimenez <dannyalxndr@gmail.com>
2014-07-11 09:18:34 -0500

    changed show page layout

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -10,13 +10,29 @@
 		@if ($post->img_path)
 			<img src="{{{ $post->img_path }}}" class="img-responsive">
 		@endif 
-
-		{{ link_to_action('PostsController@edit', 'Edit', array($post->id)) }} <br>
+		@if (Auth::check())
+			{{ link_to_action('PostsController@edit', 'Edit', array($post->id)) }} <br>
+			{{ Form::open(array('action' => array('PostsController@destroy', $post->id), 'id' => 'deleteForm', 'method' => 'DELETE')) }}
+				{{ Form::submit('Delete') }}
+			{{ Form::close() }}
+		@endif
 		{{ link_to_action('PostsController@index', 'Go to Index') }}
 
-		{{ Form::open(array('action' => array('PostsController@destroy', $post->id), 'method' => 'DELETE')) }}
-			{{ Form::submit('Delete') }}
-		{{ Form::close() }}
 	</div>
 
 @stop
+
+@section('bottomscript')
+
+	<script>
+
+		$('.deletePost').click(function(){
+			var postId = $(this).data('postid');
+			$('#deleteForm').attr('action', '/posts/' + postId);
+			if(confirm("Are you sure you want to delete post")) {
+				$('#deleteForm').submit();
+			}
+
+		});
+
+	</script>
